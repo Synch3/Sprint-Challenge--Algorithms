178581098268fc1b990f798444b63d38313cb562
Synch3 <jonsimo@aol.com>
2019-05-03 14:27:03 -0400

    robot sort finished

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -98,18 +98,23 @@ class SortingRobot:
         """
         # Fill this out
         self.swap_item()
-        if self.can_move_right() == True:
-            self.move_right()
-            if self.compare_item() == 1:
-                self.swap_item()
+        while(1 == 1):
+            self.set_light_off()
+            while self.can_move_right():
+                self.move_right()
+                if(self.compare_item()==1):
+                    self.swap_item()
+                    self.set_light_on()
                 self.move_left()
                 self.swap_item()
                 self.move_right()
-            else:
                 self.swap_item()
-        elif self.can_move_left() == True:
-            self.move_left()
-        else:
+            self.swap_item()
+            if (self.light_is_on()== False):
+                return 1
+            while(self.can_move_left()):
+                self.move_left()
+            self.swap_item()
             print('Done!')
 
 
